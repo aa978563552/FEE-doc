3bca18b15ca41d86d33394151ffe28fc306163f5
wangqiang <wangqiang025@lianjia.com>
2020-04-12 11:26:09 +0800

    fix: 图片引用路径问题

---
+++ assets/js/4.a50e9400.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{255:function(n,t,e){"use strict";e.r(t);var o={data:function(){return{}},mounted:function(){console.log(this.$page)}},u=e(17),c=Object(u.a)(o,(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("Content")],1)}),[],!1,null,null,null);t.default=c.exports}}]);
